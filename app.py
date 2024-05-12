c5fc9cb2d9527ccdc0e275c7be0fcfbc81f7167b
Nguyễn Huy Hoàng <115699781+hoanghelloworld@users.noreply.github.com>
2024-05-12 22:07:17 +0700

    update

--- app.py
+++ app.py
@@ -1,44 +1,30 @@
-from flask import Flask, request, render_template
-import PIL
-
-if int(PIL.__version__[0]) < 9:
-    print('Mandatory restart: Execute this cell again!')
-    import os
-    os.kill(os.getpid(), 9)
+from flask import Flask, render_template, request, jsonify
+from PIL import Image
+from pix2tex import cli as pix2tex
+import io
 
 app = Flask(__name__)
-app.static_folder = 'static'
-
-def upload_files():
-    from google.colab import files
-    from io import BytesIO
-
-    uploaded = files.upload()
-    return [(name, BytesIO(b)) for name, b in uploaded.items()]
-
-from pix2tex import cli as pix2tex
-from PIL import Image
-
 model = pix2tex.LatexOCR()
 
-from IPython.display import HTML, Math
-
-display(HTML("<script src='https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/"
-             "latest.js?config=default'></script>"))
-
-table = r'\\begin{array} {l|l} %s \\end{array}'
-
 @app.route('/')
-def home():
-    imgs = upload_files()
-    predictions = []
-    for name, f in imgs:
-        img = Image.open(f)
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'})
+    
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No file selected'})
+    
+    try:
+        img = Image.open(io.BytesIO(file.read()))
         math = model(img)
-        print(math)
-        predictions.append('\\mathrm{%s} & \\displaystyle{%s}' % (name, math))
-    latex_table = Math(table % '\\\\\\'.join(predictions))
-    return render_template('index.html', latex_table=latex_table)
+        return render_template('index.html', latex=math)
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
